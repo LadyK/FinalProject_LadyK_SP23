01f37534afc42814316f6b7a7fedf01129d25614
LadyK <LadyK@users.noreply.github.com>
2023-04-24 13:48:58 -0400

    first push
    here are my slides and my demo folder of my kick butt project for this class.

---
+++ one/Sketch.js
@@ -0,0 +1,49 @@
+let monster;
+let vader;
+let x, y, rotation;
+
+function preload(){
+  //monster = loadImage('data/Monster_300_204.png');  // the exact path for the file, from the sketch.js file
+  vader = loadImage('data/legoVader.png');
+
+}
+
+
+function setup() {
+  createCanvas(800, 800);
+
+  loadImage('data/Monster_300_204.png', m => {
+    monster = m; console.log("loaded");
+  }, console.log("failed"));
+
+ 
+/*
+   loadImage('data/legoVader.png', img => {
+    image(img, 200, 200);
+  });
+  */
+}
+
+function draw() {
+  background(0);
+  image(vader, 100, 100);
+
+  push();
+  translate(200, 200);
+  scale(.5);
+  image(vader, 200, 200);
+  pop();
+
+  if(mouseIsPressed){
+     image(monster, mouseX - 100, mouseY - 100);
+  }
+ 
+
+
+}
+
+
+
+
+
+
